4cea5bade29340e8c704e73deda576e7c8569acb
Timo Cramer <timo.cramer@tu-dortmund.de>
2013-11-20 21:49:12 +0100

    add the noreturn attribute to functions that call exit

--- errors.h
+++ errors.h
@@ -5,11 +5,16 @@
 
 #include "gbparse.h"
 
-void no_memory(void);
+#ifndef __GNUC__
+#undef __attribute__
+#define __attribute__(xyz)
+#endif
 
-void gbasm_error(const char *, ...);
+void no_memory(void) __attribute__((noreturn));
+
+void gbasm_error(const char *, ...) __attribute__((noreturn));
 void gbasm_warning(const char *, ...);
 
-void location_error(YYLTYPE, char *, ...);
+void location_error(YYLTYPE, char *, ...) __attribute__((noreturn));
 void location_warning(YYLTYPE, char *, ...);
 #endif
