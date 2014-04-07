b903b0e0b2cb7981a41d8ec3363a49bba3c380a0
Timo Cramer <timo.cramer@tu-dortmund.de>
2014-04-07 11:55:42 +0200

    use C11 _Noreturn instead of GNUC-attribute

--- errors.h
+++ errors.h
@@ -5,16 +5,11 @@
 
 #include "gbparse.h"
 
-#ifndef __GNUC__
-#undef __attribute__
-#define __attribute__(xyz)
-#endif
+_Noreturn void no_memory(void);
 
-void no_memory(void) __attribute__((noreturn));
-
-void gbasm_error(const char *, ...) __attribute__((noreturn));
+_Noreturn void gbasm_error(const char *, ...);
 void gbasm_warning(const char *, ...);
 
-void location_error(YYLTYPE, char *, ...) __attribute__((noreturn));
+_Noreturn void location_error(YYLTYPE, char *, ...);
 void location_warning(YYLTYPE, char *, ...);
 #endif
