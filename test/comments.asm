c8f06cccb5a6bb971ea398abcd1c81a6e59797dd
Timo Cramer <timo.cramer@tu-dortmund.de>
2014-05-07 20:35:48 +0200

    add test for comments

---
+++ test/comments.asm
@@ -0,0 +1,7 @@
+add a     ; this is a comment in a line
+add b
+; this is a comment at the beginning of a line
+add c
+	; this is a comment after a tab
+add d
+; this comment doesn't end with a newline
