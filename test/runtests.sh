f01f3333a93fa2cb62f3f28847c2639e74948d10
Timo Cramer <timo.cramer@tu-dortmund.de>
2014-11-23 21:35:20 +0100

    runtests.sh now runs all tests and does not exit after the first failed test

--- test/runtests.sh
+++ test/runtests.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+errors=0
 for x in *.asm; do
 	diff -q "${x/.asm/.gb}" "${x/.asm/.expect}"
 	if [ $? != 0 ]; then
-		exit 1
+		errors=$((errors + 1))
 	fi
 done
 
-echo "all files seem to be like expected"
-exit 0
+echo "all tests done"
+echo "errors: $errors"
+if [ $errors = 0 ]; then
+	exit 0
+else
+	exit 1
+fi
